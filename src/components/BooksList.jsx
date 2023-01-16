2e51a8ae99e457c7069c3682156885024a06a98e
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 20:18:56 +0530

    Add Book List Component

---
+++ src/components/BooksList.jsx
@@ -0,0 +1,12 @@
+import Book from "./Book"
+
+const BooksList = () => {
+  return (
+    <div className="py-10">
+        <h2 className="text-4xl font-bold py-4">Your Books List</h2>
+        <Book />
+    </div>
+  )
+}
+
+export default BooksList
