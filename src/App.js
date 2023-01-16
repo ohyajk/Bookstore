463fe21aed270c6a31d431052c620fd750ab6ca3
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 18:28:49 +0530

    Setup Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BooksPage from "./pages/BooksPage";
+import CategoriesPage from "./pages/CategoriesPage";
+
 const App = () => {
   return (
-    <div  className="text-3xl font-bold underline">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BooksPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
