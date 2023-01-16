4fd824f37c476b4ac92f24c2fcf6cedfd00e7f58
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 20:19:07 +0530

    Update Navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="shadow-md shadow-white/30 flex justify-between items-center px-[10vh] py-4">
         <nav>
-            <h1 className="logo text-4xl font-semibold">JK's BOOKSTORE</h1>
+            <h1 className="logo text-4xl font-semibold text-fuchsia-500">JK's BOOKSTORE</h1>
         </nav>
         <ul className="flex gap-4">
             <Link to='/'>BOOKS</Link>
