a1b6b14da1fcb4eafa8facf5dedd0191e2308b1f
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 18:09:23 +0530

    Fix Tailwind Error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div>App</div>
+    <div  className="text-3xl font-bold underline">App</div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  "./src/**/*.{js,jsx,ts,tsx}",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
