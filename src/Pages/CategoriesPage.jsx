cd71abf6cdf379a0d67b1ce9398efebdfc1ea682
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 20:19:41 +0530

    Add Check Status Button

--- src/Pages/CategoriesPage.jsx
+++ src/Pages/CategoriesPage.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const CategoriesPage = () => {
   return (
-    <div>CategoriesPage</div>
+    <div className="flex justify-center py-8">
+        <button className="border-2 border-fuchsia-500 p-2 cursor-pointer rounded-lg">Check Status</button>
+    </div>
   )
 }
 
