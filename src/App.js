5eed846babd3ce1e421e367f01524e5f893adfeb
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 20:28:09 +0530

    Fix Eslint Errors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import BooksPage from "./pages/BooksPage";
-import CategoriesPage from "./pages/CategoriesPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import BooksPage from './pages/BooksPage';
+import CategoriesPage from './pages/CategoriesPage';
 
-const App = () => {
-  return (
-    <BrowserRouter>
+const App = () => (
+  <BrowserRouter>
     <Navbar />
-      <Routes>
-        <Route path="/" element={<BooksPage />} />
-        <Route path="/categories" element={<CategoriesPage />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+    <Routes>
+      <Route path="/" element={<BooksPage />} />
+      <Route path="/categories" element={<CategoriesPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
