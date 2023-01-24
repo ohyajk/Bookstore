9bf987d1ed93d713f2baa7ee5f43fa9d5b1bc3de
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-24 23:27:27 +0530

    get Book list by map

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,10 +1,16 @@
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
-const BooksList = () => (
-  <div className="py-10">
-    <h2 className="text-4xl font-bold py-4">Your Books List</h2>
-    <Book />
-  </div>
-);
+const BooksList = () => {
+  const list = useSelector((state) => state.bookRed);
+  return (
+    <div className="py-10">
+      <h2 className="text-4xl font-bold py-4">Your Books List</h2>
+      {list.map((book) => (
+        <Book title={book.title} author={book.author} key={book.id} id={book.id} />
+      ))}
+    </div>
+  );
+};
 
 export default BooksList;
