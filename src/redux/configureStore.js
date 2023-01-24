054890876614dd27a28648c9557168b0bf34e23f
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-24 19:00:40 +0530

    Add store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import bookRed from './books/books';
+import categoryRed from './categories/categories';
 
-const store = configureStore({
-  reducer: {
-    books: [],
-  },
+const reducer = combineReducers({
+  bookRed,
+  categoryRed,
 });
 
+const store = configureStore({ reducer });
 export default store;
