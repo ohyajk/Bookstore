d2b3897c8a86429e185deea3c84a62b55a042ac7
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-24 23:38:33 +0530

    Fix error

--- src/Pages/CategoriesPage.jsx
+++ src/Pages/CategoriesPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { checkStatus } from '../redux/categories.js/categories';
+import { checkStatus } from '../redux/categories/categories';
 
 const CategoriesPage = () => {
   const dispatch = useDispatch();
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => (
   <div className="shadow-md shadow-white/30 flex justify-between items-center px-[10vh] py-4">
     <nav>
-      <h1 className="logo text-4xl font-semibold text-fuchsia-500">JK's BOOKSTORE</h1>
+      <h1 className="logo text-4xl font-semibold text-fuchsia-500">JK`&apos;`s BOOKSTORE</h1>
     </nav>
     <ul className="flex gap-4">
       <Link to="/">BOOKS</Link>
